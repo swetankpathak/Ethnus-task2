54519fe52376568e36076b53a2b32530d32267da
swetankpathak <swetankpathak26@gmail.com>
2023-06-21 14:39:53 +0530

    commit name

--- src/Task6.js
+++ src/Task6.js
@@ -7,7 +7,7 @@ function Task6() {
         <div className="portfolio px-3 py-3">
             <table cellspacing="35">
                 <tr>
-                    <td><img className="profilePicture" src="https://raw.githubusercontent.com/guptasajal411/cv/main/images/sajalGupta.png" alt="Profile Picture" height="250" /></td>
+                    <td><img className="profilePicture" src=" "
                     <td>
                         <h1 className="name">Swetank Pathak</h1>
                         <p><em>CS Senior at VIT Bhopal. </em></p>
